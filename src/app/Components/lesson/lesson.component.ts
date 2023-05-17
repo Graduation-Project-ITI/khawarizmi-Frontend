9d0cf6a958611ec966ade59c9bec9182e3d47e28
Abanoub Saleh <abanoubsaleh56@gmail.com>
2023-05-18 00:07:31 +0300

    Abanoub

--- src/app/Components/lesson/lesson.component.ts
+++ src/app/Components/lesson/lesson.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LessonService } from 'src/app/services/lesson.service';
-import { EditLessonTitleComponent } from '../edit-lesson-title/edit-lesson-title.component';
 import { ChangeLessonVideoComponent } from '../change-lesson-video/change-lesson-video.component';
-
+import { EditLessonTitleComponent } from '../edit-lesson-title/edit-lesson-title.component';
 @Component({
   selector: 'app-lesson',
   templateUrl: './lesson.component.html',
