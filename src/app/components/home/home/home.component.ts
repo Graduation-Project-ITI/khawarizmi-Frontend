061bdcdabc5084165ac2a159b49617c2e8fbb567
Mahamed Magdy <m7md96mgdy@gmail.com>
2023-06-18 18:42:46 +0300

    auth

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit, OnDestroy{
+  constructor() {
+    console.log("ctor of home")
+  }
+  ngOnInit(): void {
+    console.log("onInit of home")
+  }
+  ngOnDestroy(): void {
+    console.log("destroy home")
+  }
 }
