6577ac2170c8c504023303ccde703e1901f416a9
AbdallahNagy <abdallahnagy773@gmail.com>
2023-06-24 12:55:55 +0300

    fix static content

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActiveService } from 'src/app/services/RegisterService/active.service';
+import { CreateCourseService } from 'src/app/services/create-course.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,18 @@ import { ActiveService } from 'src/app/services/RegisterService/active.service';
 export class HomeComponent implements OnInit {
 
   isAuthenticated : any;
-
-  constructor(private actServ: ActiveService) {
+  categories: any[] | null = null;
+  constructor(private actServ: ActiveService, private course: CreateCourseService) {
 
   }
 
   ngOnInit(): void {
     this.isAuthenticated = this.actServ.isLoggedIn();
+  }
+
+  getCagegories(){
+    this.course.getCategories().subscribe({
+      next: res => console.log(res)
+    })
   }
 }
