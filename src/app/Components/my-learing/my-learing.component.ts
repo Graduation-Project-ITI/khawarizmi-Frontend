ce7a242451304c6021b3386a507e1decd23b839a
AbdallahNagy <abdallahnagy773@gmail.com>
2023-06-24 16:47:51 +0300

    latest dev

--- src/app/Components/my-learing/my-learing.component.ts
+++ src/app/Components/my-learing/my-learing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LearningService } from 'src/app/Services/MyLearningService/learning.service';
+import { LearningService } from 'src/app/services/MyLearningService/learning.service';
 
 import { environment } from 'src/environments/environment.development';
 
@@ -12,11 +12,10 @@ export class MyLearingComponent implements OnInit {
  id:any;
  courses:any;
  baseurl=environment.baseURL;
+ isEmpty: boolean = false;
   constructor(private myservice:LearningService) {
-    
     //this.id= myId.snapshot.params['UserId'];
-this.id=localStorage.getItem("userId");
-    
+    this.id=localStorage.getItem("userId");    
   }
 
   ngOnInit(): void {
@@ -24,13 +23,14 @@ this.id=localStorage.getItem("userId");
       {
         next:(res)=>{ console.log(res);
           this.courses=res;
+          if(this.courses.length == 0) this.isEmpty = true;
+          else this.isEmpty = false;
         },
         error:(err)=>{
           console.log(err)
         }
       }
     );
-
   }
   GetCourses(data:string)
   {
@@ -38,6 +38,8 @@ this.id=localStorage.getItem("userId");
       {
         next:(res)=>{ console.log(res);
           this.courses=res;
+          if(this.courses.length == 0) this.isEmpty = true;
+          else this.isEmpty = false;
         },
         error:(err)=>{
           console.log(err)
